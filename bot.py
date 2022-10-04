9044bc6ebf0da40272d28c7360ddd3f5105c76a3
Harrison Jumper <jumper.ha@northeastern.edu>
2022-10-04 13:51:11 -0400

    removed birthday command

--- bot.py
+++ bot.py
@@ -127,11 +127,6 @@ async def on_message(message):
   if message.content.startswith('!commands'):
     #Output the new amsterdam message.
     await message.channel.send("alliance_chat\ncommands\ndevs\nhelp\nnew_amsterdam\nwinners")
-  
-  #If the message starts with '!birthday'
-  if message.content.startswith('!birthday'):
-    #Output the video link.
-    await message.channel.send("https://www.youtube.com/watch?v=Iy_w2_C1zlc")
 
 #run bot
 client.run(TOKEN)
