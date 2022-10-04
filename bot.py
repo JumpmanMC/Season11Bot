4b519556b4093a0a074802a62a8912ada246c2ec
Harrison Jumper <jumper.ha@northeastern.edu>
2022-10-04 18:05:57 -0400

    host fix

--- bot.py
+++ bot.py
@@ -98,18 +98,18 @@ async def on_message(message):
     #send the documentation
     await message.channel.send("https://docs.google.com/document/d/1mRM8xl5f2_zhOgsapYVhOHUXql13RGacO_wCArEuyuU/edit?usp=sharing")
   
-  #If the message starts with '!host'
-  if message.content.startswith('!host'):
+  #If the message starts with '!hosts'
+  if message.content.startswith('!hosts'):
     #Create the output table of hosts.
     print("hosts")
     output = t2a(
       header=["Seasons", "Host"],
-      body=[[11-12, 'Payton McAlice'],
-            [9-10, 'Lauren Murphy'],
-            [7-8, 'Andy Lok'], 
-            [6-7, 'Andrew Carlson'],
-            [3-5, 'Rob'],
-            [1-2, 'Casey Abel'],],
+      body=[['11-12', 'Payton McAlice'],
+            ['9-10', 'Lauren Murphy'],
+            ['7-8', 'Andy Lok'], 
+            ['6-7', 'Andrew Carlson'],
+            ['3-5', 'Rob'],
+            ['1-2', 'Casey Abel'],],
       style=PresetStyle.thin_compact,
       first_col_heading=True
     )
